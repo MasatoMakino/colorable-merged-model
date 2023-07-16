58510b3e2a1df45736a0b7347508b496abbfb053
MasatoMakino <makino.masato.g@gmail.com>
2023-07-16 17:19:35 +0900

    remove : unused function

--- __test__/MergedModelUtil.spec.ts
+++ __test__/MergedModelUtil.spec.ts
@@ -1,8 +1,12 @@
 import { readGeometryCount } from "../src/index.js";
-import { BoxGeometry, BufferGeometry } from "three";
+import { BoxGeometry, BufferGeometry, PlaneGeometry } from "three";
 
 describe("MergedModelUtil", () => {
-  test("count position", () => {
+  test("count plane", () => {
+    const geometry = new PlaneGeometry(1, 1, 1, 1);
+    expect(readGeometryCount(geometry)).toBe(4);
+  });
+  test("count box", () => {
     const geometry = new BoxGeometry(1, 1, 1, 1, 1, 1);
     expect(readGeometryCount(geometry)).toBe(24);
   });
