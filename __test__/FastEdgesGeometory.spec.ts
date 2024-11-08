f042aa1b91f9456e8a8c1ac3609f97d4a790cb73
MasatoMakino <makino.masato.g@gmail.com>
2024-11-09 00:08:26 +0900

    add : implement BoxGeometry support in FastEdgesGeometry tests and demo

--- __test__/FastEdgesGeometory.spec.ts
+++ __test__/FastEdgesGeometory.spec.ts
@@ -1,6 +1,16 @@
 import { describe, expect, it } from "vitest";
 import { FastEdgesGeometry } from "../src";
-import { BufferGeometry, EdgesGeometry, TorusKnotGeometry } from "three";
+import {
+  BoxGeometry,
+  BufferGeometry,
+  ConeGeometry,
+  CylinderGeometry,
+  EdgesGeometry,
+  RingGeometry,
+  SphereGeometry,
+  TorusGeometry,
+  TorusKnotGeometry,
+} from "three";
 
 describe("FastEdgesGeometry", () => {
   it("should correctly initialize FastEdgesGeometry constructor", () => {
@@ -21,12 +31,20 @@ describe("FastEdgesGeometry", () => {
     expect(edges.parameters.thresholdAngle).toBe(1);
   });
 
-  it("should match the position attribute of FastEdgesGeometry with EdgesGeometry", () => {
-    const geo = new TorusKnotGeometry();
+  const checkPosition = (geo: BufferGeometry) => {
     const fastEdges = new FastEdgesGeometry(geo);
     const edges = new EdgesGeometry(geo);
     expect(fastEdges.attributes.position.array).toStrictEqual(
       edges.attributes.position.array,
     );
+  };
+  it("should match the position attribute of FastEdgesGeometry with EdgesGeometry", () => {
+    checkPosition(new TorusKnotGeometry());
+    checkPosition(new TorusGeometry());
+    checkPosition(new SphereGeometry());
+    checkPosition(new CylinderGeometry());
+    checkPosition(new ConeGeometry());
+    checkPosition(new RingGeometry());
+    checkPosition(new BoxGeometry(10, 11, 12));
   });
 });
---
+++ demoSrc/demo_FastEdgesGeometory_Box.ts
@@ -0,0 +1,86 @@
+import {
+  AdditiveBlending,
+  BoxGeometry,
+  BufferGeometry,
+  EdgesGeometry,
+  LineBasicMaterial,
+  LineSegments,
+  TorusKnotGeometry,
+} from "three";
+import { generateScene } from "./GenerateScene";
+import { FastEdgesGeometry } from "../src";
+import GUI from "lil-gui";
+
+const onDomContentsLoaded = async () => {
+  const { scene, camera } = generateScene();
+
+  const geometry = new BoxGeometry(10, 10, 10);
+  const { edges, fastEdges } = generateEdges(geometry, 0);
+
+  const edgesMesh = new LineSegments(
+    edges,
+    new LineBasicMaterial({ color: 0xff0000, blending: AdditiveBlending }),
+  );
+  const fastEdgesMesh = new LineSegments(
+    fastEdges,
+    new LineBasicMaterial({ color: 0x00ffff, blending: AdditiveBlending }),
+  );
+  edgesMesh.position.x = -20;
+  fastEdgesMesh.position.x = 20;
+
+  camera.position.z = 60;
+  scene.add(edgesMesh, fastEdgesMesh);
+
+  const gui = new GUI();
+  const obj = {
+    distance: 20,
+    thresholdAngle: 0,
+  };
+  gui.add(obj, "distance", 0, 20, 0.05).onChange((value: number) => {
+    edgesMesh.position.x = -value;
+    fastEdgesMesh.position.x = value;
+  });
+  gui.add(obj, "thresholdAngle", 0, 10, 0.1).onChange((value: number) => {
+    regenerateEdges();
+  });
+
+  const regenerateEdges = () => {
+    const geometry = new BoxGeometry(10, 10, 10);
+    const { edges, fastEdges } = generateEdges(geometry, obj.thresholdAngle);
+    edgesMesh.geometry.dispose();
+    fastEdgesMesh.geometry.dispose();
+    edgesMesh.geometry = edges;
+    fastEdgesMesh.geometry = fastEdges;
+  };
+};
+
+const generateEdges = (
+  geometry: BufferGeometry,
+  thresholdAngle: number = 1,
+) => {
+  const startEdges = performance.now();
+  const edges = new EdgesGeometry(geometry, thresholdAngle);
+  const endEdges = performance.now();
+  const timeEdges = endEdges - startEdges;
+  console.log(`EdgesGeometry: ${timeEdges}ms`);
+
+  const start = performance.now();
+  const fastEdges = new FastEdgesGeometry(geometry, thresholdAngle);
+  const end = performance.now();
+  const time = end - start;
+  console.log(`FastEdgesGeometry: ${time}ms`);
+
+  if (timeEdges / time < 1) {
+    console.warn(
+      `🚫 FastEdgesGeometry is slower than EdgesGeometry. FastEdgesGeometry is ${timeEdges / time} times slower than EdgesGeometry.`,
+    );
+  } else {
+    console.log(
+      `👍 FastEdgesGeometry is ${timeEdges / time} times faster than EdgesGeometry.`,
+    );
+  }
+
+  return { edges, fastEdges };
+};
+
+window.onload = onDomContentsLoaded;
